a1f34aa0404a9f47f707c4ff79c554b45212fcb9
sveri <sveri80@gmail.com>
2019-08-25 22:06:29 +0200

    adapted get initial data to also include shared lists

--- migrators/postgres/5-share-hashes-entry.up.sql
+++ migrators/postgres/5-share-hashes-entry.up.sql
@@ -3,7 +3,8 @@
 
 CREATE TABLE share_hashes (
 mobile_clients_id uuid NOT NULL,
-hash TEXT NOT NULL
+hash TEXT NOT NULL,
+list_id uuid NOT NULL
 );
 
 ALTER TABLE share_hashes OWNER TO shoplibackend;
